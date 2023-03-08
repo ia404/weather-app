1cb7abbb71ea5d1b557d3b30e9ae9a2bccb04746
ia404 <imran.a10outlook.com>
2023-03-08 10:04:59 +0000

    second commit

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,21 +1,15 @@
 // import preact
 import { h, render, Component } from 'preact';
-	
 export default class Button extends Component {
-
 	// rendering a function when the button is clicked
 	render() {
 		let cFunction = this.props.clickFunction;
-		if(typeof cFunction !== 'function'){
-			cFunction = () => {
-				console.log("passed something as 'clickFunction' that wasn't a function !");
-			}
+		if (typeof cFunction !== 'function'){
+			cFunction = () => {};	
 		}
 		return (
 			<div>
-				<button onClick={cFunction}>
-					Display Weather
-				</button>
+				<button onClick={cFunction}> </button>
 			</div>
 		);
 	}
--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -1,70 +1,118 @@
 // import preact
 import { h, render, Component } from 'preact';
-// import stylesheets for ipad & button
+// import stylesheets for iphone
 import style from './style';
-import style_iphone from '../button/style_iphone';
+import style_button from '../button/style_button';
+import style_leftarrow from '../button/left_arrow';
+import style_rightarrow from '../button/right_arrow';
+
 // import jquery for API calls
 import $ from 'jquery';
 // import the Button component
 import Button from '../button';
 
+
+
+// API KEY: 55c7bf54637844c3556adeff289f1b44
 export default class Iphone extends Component {
+	
 //var Iphone = React.createClass({
 
 	// a constructor with initial set states
+
 	constructor(props){
 		super(props);
 		// temperature state
+		this.pages = ["home", "forecast", "warning"];
 		this.state.temp = "";
-		// button display state
-		this.setState({ display: true });
+		this.state.page = this.pages[0];
 	}
 
-	// a call to fetch weather data via wunderground
+	//create pages for the iphone
+	// fetch weather data from openweathermap.org
 	fetchWeatherData = () => {
-		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=cf17e23b1d108b29a4d738d2084baf5";
+		// API call to openweathermap.org
+		const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=55c7bf54637844c3556adeff289f1b44";
+		// retrieve the data from the API call and parse it to JSON
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
-			success : this.parseResponse,
+			success : this.parseResponse, 
 			error : function(req, err){ console.log('API call failed ' + err); }
-		})
-		// once the data grabbed, hide the button
-		this.setState({ display: false });
+		});
+
+	}
+
+	//function to change the page forward
+	changePageForward = () => {
+		let i = this.pages.indexOf(this.state.page); //checks current index of the page array
+		//as long as the index is not on the last page, it can be changed.
+		if (i !== this.pages.length-1) {
+			this.setState({ page: this.pages[i+1] }); 
+		}
+	}
+
+	//function to change the page backward
+	changePageBackward = () => {
+		let i = this.pages.indexOf(this.state.page); //checks current index of the page array 
+		//if the index is either 1 or 2 then it can be moved backwards.
+		if (i !== 0 && i < this.pages.length) { 
+			this.setState({ page: this.pages[i-1] });
+		} 
 	}
 
 	// the main render method for the iphone component
 	render() {
+		//fetch weatherdata
+		//this.fetchWeatherData();
+
 		// check if temperature data is fetched, if so add the sign styling to the page
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
-		
-		// display all weather data
 		return (
 			<div class={ style.container }>
+				
 				<div class={ style.header }>
-					<div class={ style.city }>{ this.state.locate }</div>
-					<div class={ style.conditions }>{ this.state.cond }</div>
-					<span class={ tempStyles }>{ this.state.temp }</span>
+					<div class= { style.today }> Today </div>
+				   { this.state.page === "home" && <img class={ style.header } src="../assets/backgrounds/mountain.png" alt="mountain" /> }
+
 				</div>
+
 				<div class={ style.details }></div>
-				<div class= { style_iphone.container }> 
-					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
+				{ this.state.page === "home" && <div class={ style.home }>
+					<div> Home </div>
+					<span class={ tempStyles }> { this.state.temp } </span>
+				</div> }
+				
+				{ this.state.page === "forecast" && <div class={ style.forecast }>
+					<div> Forecast </div>
+				</div> }
+
+				{ this.state.page === "warning" && <div class={ style.warning }>
+					<div> Warning </div>
+				</div> }
+				
+				<div class={ style.footer }>
+					<div className={ style_leftarrow.container }>
+						<Button  className={ style_leftarrow.button } id="left" clickFunction={ this.changePageBackward }/>
+					</div>
+					<div className={ style_button.container }>
+						<Button  className={ style_button.button }/> 
+						<Button  className={ style_button.button }/> 
+						<Button  className={ style_button.button }/> 
+					</div>
+					<div className={ style_rightarrow.container }>
+						<Button  className={ style_rightarrow.button } id="right" clickFunction={ this.changePageForward }/>
+					</div>
 				</div>
 			</div>
 		);
 	}
 
 	parseResponse = (parsed_json) => {
-		var location = parsed_json['name'];
-		var temp_c = parsed_json['main']['temp'];
-		var conditions = parsed_json['weather']['0']['description'];
-
+		//LOCATION: var location = parsed_json['name'];
+		let temp_c = parsed_json['main']['temp'];
+		//CURRENT WEATHER CONDITION: var conditions = parsed_json['weather']['0']['description'];
 		// set states for fields so they could be rendered later on
-		this.setState({
-			locate: location,
-			temp: temp_c,
-			cond : conditions
-		});      
+		this.setState({ temp: temp_c });      
 	}
 }
